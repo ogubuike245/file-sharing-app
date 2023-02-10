d4f2af15f33cece9d4e525c1f2a9f71fb9235700
ogubuike <68479614+ogubuike245@users.noreply.github.com>
2023-02-10 08:24:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ connectToDatabase(app);
 
 app.use(express.static("./dist"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compression());
 app.use(helmet());
